87f6db3a621279543c0d2038b894d33735e950f5
Razio1 <radek_mucha@op.pl>
2016-05-30 02:09:18 +0200

    30.05.2016 02:09

--- Statements.cpp
+++ Statements.cpp
@@ -1,3 +1,5 @@
+#include "Statements.h"
+
 #include <iostream>
 
 using namespace std;
@@ -7,9 +9,10 @@ void extra_car(int plan_cars)
 	cout<<"Wagon dodany z rezerwy taborowej: nr"<<plan_cars/100<<endl;
 }
 
-void no_extra_car()
+void no_extra_car(int no)
 {
 	cout<<"Przepraszamy, brak dostępnych miejsc"<<endl;
+	cout<<"Liczba pasazerow bez miesjca do siedzenia: "<<no<<endl;
 }
 
 void info(int plan_cars, int extra_cars, int quantity)
@@ -18,3 +21,5 @@ void info(int plan_cars, int extra_cars, int quantity)
 	cout<<"Przygotowana rezerwa: "<<extra_cars<<endl;
 	cout<<"Planowana ilosc pasazerow: "<<quantity<<endl<<endl;
 }
+
+
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "Reservations.h"
 #include "Reservations.cpp"
+#include "Statements.h"
 #include "Statements.cpp"
 
 #include <iostream> 
@@ -15,14 +16,16 @@ int main(int argc, char *argv[])
 {
     srand(time(NULL));
 
-	int quantity=random_value(1000, 1);
+	int quantity=random_value(70, 1);
 	int parameters[3];
-	int plan_cars=random_value(5, 3)*100;
-	int extra_cars=random_value(5, 1);
+	int plan_cars=random_value(2, 1)*100;
+	int extra_cars=random_value(10, 1);
 	int temp=0;
+	int temp1=plan_cars;
+	bool ending=false;
 
 	ifstream train;
-	train.open("train.txt");
+	train.open("train3.txt");
     ofstream diagram;
     diagram.open("diagram.txt");
 
@@ -36,10 +39,16 @@ int main(int argc, char *argv[])
 
 	int stations = atoi(data[0].c_str());
 
+	diagram<<endl<<data[1]<<endl<<data[2]<<endl<<data[4]<<endl<<data[3+stations]<<endl<<endl;
+	cout<<endl<<data[1]<<endl<<data[2]<<endl<<data[4]<<endl<<data[3+stations]<<endl<<endl;
+
 	info(plan_cars, extra_cars, quantity);
+	diagram<<endl<<"Planowa ilosc wagonow: "<<plan_cars/100<<endl;
+	diagram<<"Przygotowana rezerwa: "<<extra_cars<<endl;
+	diagram<<"Planowana ilosc pasazerow: "<<quantity<<endl<<endl;
 
 	vector < vector < int > > Occupancy;
-	vector < int > occupancy(3);
+	vector < int > occupancy(4);
 
 	for (int i=0; i<plan_cars; i++)
     {
@@ -51,7 +60,6 @@ int main(int argc, char *argv[])
 
      for(int i=0; i<quantity; i++)
      {
-     	if((i%10>6)||(i%10==0)) quantity++;
      	if(i+1>plan_cars)
      	{
      		if(extra_cars>0)
@@ -64,10 +72,13 @@ int main(int argc, char *argv[])
     	              Occupancy.push_back(occupancy);
                      }
                      extra_car(plan_cars);
+                     diagram<<"Wagon dodany z rezerwy taborowej: nr"<<plan_cars/100<<endl;
     		}
     		else
     		{
-    			no_extra_car();
+    			no_extra_car(temp1-plan_cars/100*60);
+    			diagram<<"Przepraszamy, brak dostępnych miejsc"<<endl;
+				diagram<<"Liczba pasazerow bez miesjca do siedzenia: "<<temp1-plan_cars/100*60<<endl;
     			quantity=quantity-i;
     			break;
     		} 
@@ -75,28 +86,99 @@ int main(int argc, char *argv[])
 
      	parameters[0]=_account._start_station(stations);
      	parameters[1]=_account._end_station(stations, parameters[0]);
-     	parameters[2]=account.position()/2;
-     	for(int j=1; j<plan_cars; j++)
+     	parameters[2]=account.position();
+
+     	if((i%10>6)||(i%10==0)) quantity++;
+     	else if((parameters[2]==(6))||(parameters[2]==5))
      	{
-     		if(Occupancy[j][0]==1) ;
-     		else if(j%10>6) ;
-     		else if(j%10==0) ;
+     		for(int j=1; j<plan_cars; j++)
+     		{
+     		if(ending==false)
+     		{
+     		if(Occupancy[j][0]==1);
+     		else if((j%10==6)||(j%10==5))
+     		{
+     				Occupancy[j][0]=1;
+     				Occupancy[j][1]=parameters[0];
+     				Occupancy[j][2]=parameters[1];
+     				Occupancy[j][3]=parameters[2];
+     				ending=true;
+     				//cout<<"a";
+     			}
+     		}
+     	}
+     	}
+     	else if((parameters[2]==(4))||(parameters[2]==3))
+     	{
+     		for(int j=1; j<plan_cars; j++)
+     		{
+     		if(ending==false)
+     		{
+     		if(Occupancy[j][0]==(1)) ;
+     		else if((j%10==4)||(j%10==3))
+     		{
+     				Occupancy[j][0]=1;
+     				Occupancy[j][1]=parameters[0];
+     				Occupancy[j][2]=parameters[1];
+     				Occupancy[j][3]=parameters[2];
+     				ending=true;
+     				//cout<<"b";
+     			}
+     		}
+     	}
+     	}
+     	else if ((parameters[2]==(2))||(parameters[2]==1))
+     	{
+     		for(int j=1; j<plan_cars; j++)
+     		{
+     		if(ending==false)
+     		{
+     		if(Occupancy[j][0]==1);
+     		else if((j%10==2)||(j%10==1))
+     		{
+     				Occupancy[j][0]=1;
+     				Occupancy[j][1]=parameters[0];
+     				Occupancy[j][2]=parameters[1];
+     				Occupancy[j][3]=parameters[2];
+     				ending=true;
+     				//cout<<"c";
+     				}
+     			}
+     		}
+     	}
+     	
+     	if((i%10)>7) ;
+     	else if(ending==false)
+     		{
+     		for(int j=1; j<plan_cars; j++)
+     		{
+     		if(ending==false)
+     		{
+     		if(Occupancy[j][0]==1);
+     		else if(j%10>6);
+     		else if(j%10==0);
      		else
      		{
      				Occupancy[j][0]=1;
      				Occupancy[j][1]=parameters[0];
      				Occupancy[j][2]=parameters[1];
-     				break;
+     				Occupancy[j][3]=parameters[2];
+     				ending=true;
+     				//cout<<"d";
+     				}
      			}
+     		}
      	}
-     	//cout<<i<<" "<<Occupancy[i][1]<<" "<<Occupancy[i][2]<<"\n";
+     	ending=false;
+     	cout<<i<<endl;
      }
-     	for(int t=0; t<plan_cars; t++)
-     	{
-     		cout<<t<<" "<<Occupancy[t][1]<<" "<<Occupancy[t][2]<<"\n";
-     		diagram<<t<<" "<<data[Occupancy[t][1]+3]<<" "<<data[Occupancy[t][2]+3]<<"\n";
-     	}
-
+          	for(int t=0; t<plan_cars; t++)
+          	{
+     		if(t%100==0) diagram<<endl<<endl<<"wagon nr: "<<t/100+1<<endl;
+     		diagram<<t%100<<" "<<data[Occupancy[t][1]+3]<<" -> "<<data[Occupancy[t][2]+3]<<"\n";
+     		if(t%100==0) cout<<endl<<endl<<"wagon nr: "<<t/100+1<<endl;
+     		cout<<t%100<<" "<<Occupancy[t][1]<<" "<<Occupancy[t][2]<<" "<<Occupancy[t][3]<<"\n";
+     	    }
  }
 
 
